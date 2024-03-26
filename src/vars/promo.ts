847c49cd4e5fac86de85a8f827dd247dba09d9c3
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-03-26 18:05:45 +0530

    Promo text fixes

--- src/vars/promo.ts
+++ src/vars/promo.ts
@@ -1,5 +1,7 @@
+import { cleanUpBotMessage } from "@/utils/bot";
+
 export let promoText = "";
 
 export function setPromoText(newPromoText: string) {
-  promoText = `\n*🎺 _Sponsered Ad_*\n*${newPromoText}*`;
+  promoText = `\n*🎺 _Sponsered Ad_*\n*${cleanUpBotMessage(newPromoText)}*`;
 }
